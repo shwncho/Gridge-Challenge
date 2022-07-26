39103d1cad720c78d89f40bc1a1c466fdc60d481
simple <shwncho4@gmail.com>
2022-07-27 01:23:15 +0900

    Refactor: spring property

--- src/main/java/com/server/insta/InstaApplication.java
+++ src/main/java/com/server/insta/InstaApplication.java
@@ -1,15 +1,20 @@
 package com.server.insta;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class InstaApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(InstaApplication.class, args);
-	}
 
+		new SpringApplicationBuilder(InstaApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+
+	}
 }
