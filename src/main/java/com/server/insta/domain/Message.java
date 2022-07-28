51eeaea6b36cfd822e85e0a05ef43c723ce65fd6
simple <shwncho4@gmail.com>
2022-07-29 01:56:08 +0900

    Feat: Create Message Entity

---
+++ src/main/java/com/server/insta/domain/Message.java
@@ -0,0 +1,34 @@
+package com.server.insta.domain;
+
+import com.server.insta.config.Entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    @NotNull
+    private String content;
+
+
+}
--- src/main/java/com/server/insta/dto/request/UpdatePostRequestDto.java
+++ src/main/java/com/server/insta/dto/request/UpdatePostRequestDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public class UpdatePostRequestDto {
 
     @Schema(description = "게시물 설명")
+    @Size(max=1000, message = "최대 1000자 까지만 가능합니다.")
     private String caption;
 
     @Schema(description = "수정해서 새로 작성할 해시태그, 수정할 것 없으면 변경 안해도 됨")
